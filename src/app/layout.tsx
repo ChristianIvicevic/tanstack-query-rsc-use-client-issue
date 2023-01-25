01e0eb22aa4990ee7653a94b39dd48052a7e8ae3
Christian Ivicevic <mail@christian-ivicevic.com>
2023-01-25 23:28:58 +0100

    Setup reproduction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
-import './globals.css'
+import { Providers } from "@/app/client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +14,12 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Providers>
+          {children}
+          <ReactQueryDevtools />
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
